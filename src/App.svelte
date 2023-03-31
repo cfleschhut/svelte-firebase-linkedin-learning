b2875e906c93d2f6f7cd68991aa4ef0f92143ce0
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-03-31 19:48:45 +0200

    Add CRUD todo-list component

--- src/App.svelte
+++ src/App.svelte
@@ -8,28 +8,16 @@
     signInWithEmailAndPassword,
     signOut,
   } from 'firebase/auth'
-  import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
   import Signup from './auth/Signup.svelte'
   import Login from './auth/Login.svelte'
   import Logout from './auth/Logout.svelte'
   import ErrorList from './auth/ErrorList.svelte'
+  import TodoList from './lib/TodoList.svelte'
 
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
-
-  const db = getFirestore()
-
-  const readData = async () => {
-    const querySnapshot = await getDocs(collection(db, 'cars'))
-    querySnapshot.forEach((doc) => {
-      console.log({
-        id: doc.id,
-        ...doc.data(),
-      })
-    })
-  }
 
   const auth = getAuth()
   let errors = []
@@ -83,8 +71,6 @@
       if (user) {
         session.set({ user })
         console.log(user.uid, 'signed in')
-
-        readData()
       } else {
         session.set({ user: null })
         console.log('not signed in')
@@ -94,15 +80,16 @@
 </script>
 
 <main>
-  {#if $session && $session.user == null}
+  {#if $session && $session.user != null}
+    <TodoList />
+    <Logout on:logout={handleAuth} />
+  {:else}
     <Login on:signin={handleAuth} />
     <Signup on:signup={handleAuth} />
-
     <ErrorList {errors} />
-  {:else}
-    <Logout on:logout={handleAuth} />
   {/if}
 
+  <h1>Vite + Svelte</h1>
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -111,21 +98,10 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -140,8 +116,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,148 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    onSnapshot,
+  } from 'firebase/firestore'
+
+  interface Todo {
+    id: string
+    title: string
+    completed: boolean
+  }
+
+  const db = getFirestore()
+  const colRef = collection(db, 'todos')
+
+  let todos = []
+  let newTodoTitle = ''
+
+  onSnapshot(colRef, (querySnapshot) => {
+    querySnapshot.forEach((doc) =>
+      console.log({
+        id: doc.id,
+        ...doc.data(),
+      })
+    )
+  })
+
+  const getTodos = async () => {
+    const querySnapshot = await getDocs(colRef)
+    querySnapshot.forEach((doc) => {
+      todos = [
+        {
+          id: doc.id,
+          ...doc.data(),
+        },
+        ...todos,
+      ]
+    })
+  }
+
+  const addTodo = async () => {
+    if (!newTodoTitle) return
+
+    const newTodo = {
+      title: newTodoTitle,
+      completed: false,
+    }
+
+    const newDoc = await addDoc(colRef, newTodo)
+    console.log(newDoc.id)
+    todos = [
+      ...todos,
+      {
+        id: newDoc.id,
+        ...newTodo,
+      },
+    ]
+
+    newTodoTitle = ''
+  }
+
+  const toggleCompleted = (todo: Todo) => {
+    updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed,
+    })
+  }
+
+  const deleteTodo = (todoId: string) => {
+    const todoIndex = todos.findIndex((todo) => todo.id == todoId)
+
+    todos = [...todos.slice(0, todoIndex), ...todos.slice(todoIndex + 1)]
+
+    deleteDoc(doc(db, 'todos', todoId))
+  }
+
+  onMount(async () => {
+    await getTodos()
+  })
+</script>
+
+<form on:submit|preventDefault={addTodo} class="add-todo-form">
+  <fieldset>
+    <legend>Add todos</legend>
+    <div>
+      <input bind:value={newTodoTitle} type="text" placeholder="New todo" />
+      <button>Add</button>
+    </div>
+  </fieldset>
+</form>
+
+{#if todos.length}
+  <ul class="todo-list">
+    {#each todos as todo (todo.id)}
+      <li class="todo-list-item">
+        <label>
+          <input
+            type="checkbox"
+            on:change={() => toggleCompleted(todo)}
+            checked={todo.completed}
+          />
+          {todo.title}
+        </label>
+        <button on:click={() => deleteTodo(todo.id)}>Delete</button>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+<style>
+  .add-todo-form {
+    margin: 0 0 1em;
+  }
+
+  .add-todo-form div {
+    display: flex;
+  }
+
+  .add-todo-form input[type='text'] {
+    flex: 3;
+    font-size: 100%;
+    margin: 0 8px 0 0;
+    padding: 8px;
+  }
+
+  .add-todo-form button {
+    flex: 1;
+  }
+
+  .todo-list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .todo-list-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0.25em 0;
+  }
+</style>
