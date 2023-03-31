ed2233b93c8bb06208e3625e3ac5cce49543440b
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-03-31 19:37:18 +0200

    Extract api config values into dotenv file

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import App from './App.svelte'
 import './app.css'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAH8cTRTHDT_3-QA52mJW4Xc823F56ibog',
-  authDomain: 'linkedinlearningsveltefirebase.firebaseapp.com',
-  projectId: 'linkedinlearningsveltefirebase',
-  storageBucket: 'linkedinlearningsveltefirebase.appspot.com',
-  messagingSenderId: '282619833638',
-  appId: '1:282619833638:web:d30184df16c64001c6a5e7',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 initializeApp(firebaseConfig)
